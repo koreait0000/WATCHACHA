0d9715605ae837162a31b7ba09ff832731a1e1d4
everglow <78077569+okchanho@users.noreply.github.com>
2021-07-12 22:28:23 +0900

    7/12
    테스트1

--- src/main/java/com/spring/wachacha/user/UserController.java
+++ src/main/java/com/spring/wachacha/user/UserController.java
@@ -2,12 +2,14 @@ package com.spring.wachacha.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class UserController {
 
     @GetMapping("/login")
     public String login(){
-        return "user/login";
+        return "/user/login";
     }
 }
--- src/main/webapp/WEB-INF/views/temp/tiles_temp.jsp
+++ src/main/webapp/WEB-INF/views/temp/tiles_temp.jsp
@@ -3,6 +3,7 @@
 <html>
 <head>
   <title>Title</title>
+  <link rel="stylesheet" href="../../res/css/common.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
 </head>
