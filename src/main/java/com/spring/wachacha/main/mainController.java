fad04d9aa56c9f883af8e3391b60ef1cee0d6cd4
gusals9355 <gusals9355@naver.com>
2021-07-23 19:06:20 +0900

    0723 박스오피스 랭킹 (포스터) 크롤링 완

--- src/main/java/com/spring/wachacha/main/mainController.java
+++ src/main/java/com/spring/wachacha/main/mainController.java
@@ -1,6 +1,8 @@
 package com.spring.wachacha.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 public class mainController {
 
+    @Autowired
+    private MainService mainService;
 
     @GetMapping("/mainpage")
-    public void mainpage(){ }
+    public void mainpage(Model model){
+        model.addAttribute("rankPoster",mainService.mainpage());
+    }
 
     @GetMapping("/show")
     public void show(){
