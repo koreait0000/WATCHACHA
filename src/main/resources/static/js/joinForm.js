5cccaaebecc30d10bcf42a4aa813bd15cdfc63aa
gusals9355 <gusals9355@naver.com>
2021-07-30 10:40:27 +0900

    회원가입 페이지 완

--- src/main/resources/static/js/joinForm.js
+++ src/main/resources/static/js/joinForm.js
@@ -1,28 +1,21 @@
 let form = document.querySelector('#form');
 let tel = form.tel;
-let pw = form.pw;
-let loginBtn = document.querySelector('.loginBtn');
-let regExp = /\d{3}\d{4}\d{4}/;
+let regExp = /^\d{3}\d{4}\d{4}$/;
 let verify = false;
 
 tel.addEventListener('keyup', e =>{
     if(regExp.test(tel.value)){
-        console.log('됫다');
         verify = true;
+    }else{
+        verify = false;
     }
 });
 
 function joinCheck(){
-    console.log('클릭함');
-    let email = form.email.value;
-    let telVal = form.tel.value;
-    if(verify == true){
-        console.log('됨');
-    }else{
-        console.log('안됨');
+    if(verify === false){
         let spanElem = document.createElement('span');
         spanElem.innerText = '전화번호를 확인해주세요.';
-        tel.append(spanElem);
+        tel.after(spanElem);
         return false;
     }
-};
+}
