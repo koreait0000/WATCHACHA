52402df5db0cc21501a228257b35f0bcd6c6738b
gusals9355 <gusals9355@naver.com>
2021-07-30 09:59:38 +0900

    마스터로 작업하고있었네 ㅎㅎ 다시 브런치로 이동~

--- src/main/resources/static/js/joinForm.js
+++ src/main/resources/static/js/joinForm.js
@@ -7,19 +7,22 @@ let verify = false;
 
 tel.addEventListener('keyup', e =>{
     if(regExp.test(tel.value)){
-        console.log('같다!');
+        console.log('됫다');
         verify = true;
     }
 });
 
-loginBtn.addEventListener('click', () =>{
+function joinCheck(){
+    console.log('클릭함');
     let email = form.email.value;
-    let tel = form.tel.value;
-    if(verify === true){
+    let telVal = form.tel.value;
+    if(verify == true){
         console.log('됨');
     }else{
+        console.log('안됨');
         let spanElem = document.createElement('span');
         spanElem.innerText = '전화번호를 확인해주세요.';
         tel.append(spanElem);
+        return false;
     }
-});
+};
--- src/main/webapp/WEB-INF/views/joinForm.jsp
+++ src/main/webapp/WEB-INF/views/joinForm.jsp
@@ -15,10 +15,10 @@
                 <input type="password" name="pw" placeholder="  비밀번호" required><br>
                 <input class="down" type="tel" name="tel" placeholder="  전화번호" required><br>
                 <br>
-                <button class="loginBtn">회원가입</button>
+                <button class="loginBtn" onclick="return joinCheck();">회원가입</button>
             </form>
 <%--            <div class="joinForm footer">--%>
-<%--                <label>--%>aa
+<%--                <label>--%>
 <%--                    <input class="upper" type="checkbox" required placeholder="전체약간에 동의합니다."> <br>--%>
 <%--                </label>--%>
 <%--                <label>--%>
