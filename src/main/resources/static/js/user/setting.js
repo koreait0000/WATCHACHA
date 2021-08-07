b98dc7a150df7a92306ddc73abfccc5480bc7ed3
okchanho <j33515@naver.com>
2021-08-07 17:02:54 +0900

    설정 이메일변경 모달 덜완성

--- src/main/resources/static/js/user/setting.js
+++ src/main/resources/static/js/user/setting.js
@@ -1,6 +1,21 @@
 let right_payment = document.querySelector('.right_payment');
-let change_right_modal_bg = document.querySelector('change_right_modal_bg');
+let email_change = document.querySelector('.email_change');
+let password_change = document.querySelector('.password_change');
 
 right_payment.addEventListener('click', ()=>{
-    change_right_modal_bg.classList.remove('hidden');
+    let right_modal = document.querySelector('.right_modal');
+    right_modal.classList.remove('hidden');
+    let close_modal = document.querySelector('.fas');
+    close_modal.addEventListener('click', ()=>{
+        right_modal.classList.add('hidden');
+    })
+});
+
+email_change.addEventListener('click',()=>{
+    let change_email = document.querySelector('.change_email');
+    change_email.classList.remove('hidden');
+    let close_modal = document.querySelector('.fas');
+    close_modal.addEventListener('click', ()=>{
+        change_email.classList.add('hidden');
+    })
 })
